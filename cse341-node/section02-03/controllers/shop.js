9edd35397f52151961170e92111159edfa962223
wiseman7 <wis16002@byui.edu>
2022-01-18 18:59:11 -0700

    add to cart and edit product

--- cse341-node/section02-03/controllers/shop.js
+++ cse341-node/section02-03/controllers/shop.js
@@ -1,4 +1,5 @@
 const Product = require('../models/product');
+const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
     Product.fetchAll(products => {
@@ -6,11 +7,20 @@ exports.getProducts = (req, res, next) => {
             prods: products
             , pageTitle: 'All Products'
             , path: '/products'
-            , hasProducts: products.length > 0
-            , activeShop: true
-            , productCSS: true
         });
     });
+};
+
+exports.getProduct = (req, res, next) => {
+    const prodId = req.params.productId;
+    Product.findById(prodId, product => {
+        res.render('/shop/product-detail', {
+            product: product
+            , pageTitle: product.title
+            , path: '/products'
+        });
+    });
+    
 };
 
 exports.getIndex = (req, res, next) => {
@@ -28,6 +38,14 @@ exports.getCart = (req, res, next) => {
     path: '/cart'
     , pageTitle: 'Your Cart'
     });
+};
+
+exports.postCart = (req, res, next) => {
+    const prodId = req.body.productId;
+    Product.findById(prodId, (product) => {
+        Cart.addProduct(prodId, product.price);
+    })
+    res.redirect('/cart');
 };
 
 exports.getOrders = (req, res, next) => {
